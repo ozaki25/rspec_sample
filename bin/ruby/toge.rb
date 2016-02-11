673a9be87fb44f43be8c6986f6a36a4f0443d93b
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-02-12 08:55:46 +0900

    exit処理を切り出し

--- bin/ruby/toge.rb
+++ bin/ruby/toge.rb
@@ -8,6 +8,8 @@ class Toge
 
     def load_settings_yaml
       YAML::load_file(File.join(__dir__, '../../conf/settings.yaml'))
+    rescue
+      exit_for_error
     end
 
     def first_date_string
@@ -22,20 +24,24 @@ class Toge
 
     def first_date
       Date.parse(first_date_string)
-    rescue => e
-      puts e.inspect
-      exit 1
+    rescue
+      exit_for_error
     end
 
     def last_date
       Date.parse(last_date_string)
-    rescue => e
-      puts e.inspect
-      exit 1
+    rescue
+      exit_for_error
     end
 
     def date_range
       first_date..last_date
+    end
+
+    def exit_for_error
+      puts $!
+      puts $@
+      exit 1
     end
   end
 end
